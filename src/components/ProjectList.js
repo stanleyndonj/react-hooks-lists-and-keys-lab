c8e4efaa7653692c044961795d9f14b7a31f6d7b
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-18 11:06:55 +0300

    complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,22 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+        <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies} // Pass the technologies prop
+          />
+        ))}
+      
+      </div>
+      
     </div>
   );
 }
+
 
 export default ProjectList;
